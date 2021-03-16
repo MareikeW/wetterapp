fa69348a3bfc7d5bdf5971c0308077d1cdecd249
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-03-16 11:35:45 +0100

    Whitespace unter Footer gelöscht

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 function Footer() {
     return (
-        <footer>
-            <p className="footer">&copy; 2021 Mareike Wendler</p>
+        <footer className="footer">
+            <p>&copy; 2021 Mareike Wendler</p>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
     return (
-        <header>
-            <p className="header">Suche nach dem aktuellen Wetter auf der ganzen Welt!</p>
+        <header className="header">
+            <p>Suche nach dem aktuellen Wetter auf der ganzen Welt!</p>
         </header>
     )
 }
